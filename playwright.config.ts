62bda1da38cf298422fcd0a5d0b0bc902b4544eb
aziz-bejaou <mohamed-aziz.bejaoui@enis.tn>
2024-06-06 14:15:01 +0000

    7

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,11 +10,11 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  forbidOnly: false,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
